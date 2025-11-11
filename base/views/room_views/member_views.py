1721266c65c171b2ae8c8fc0167a41dcb064bceb
cj12o <cj2004jee@gmail.com>
2025-11-11 12:59:43 +0530

    added memeber api class

---
+++ base/views/room_views/member_views.py
@@ -0,0 +1,64 @@
+from rest_framework import status,views
+from rest_framework.response import Response    
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from rest_framework.decorators import permission_classes,authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
+from ...models import Room
+
+
+# @api_view(['PATCH'])
+# @authentication_classes([TokenAuthentication])
+# @permission_classes([IsAuthenticated])
+# # def add_member(request,pk):
+#     try:
+#         """add memeber to a room for authenticated user"""
+
+#         room_id=pk
+#         obj=Room.objects.get(id=room_id)
+#         obj.members.add(request.user)
+#         return Response({"added member"},status=status.HTTP_200_OK)
+#     except Exception as e:
+#         print(f"ERROR in adding member:{str(e)}")
+#         return Response({f"error in adding member {str(e)}"},status=status.HTTP_400_BAD_REQUEST)
+
+
+class MemeberApiView(APIView):
+    permission_classes=[IsAuthenticated]
+    authentication_classes=[TokenAuthentication]
+
+    def get(self,request,pk):
+        try:
+            """add memeber to a room for authenticated user"""
+
+            room_id=pk
+            print(f"ROOM ID:{pk}")
+            obj=Room.objects.get(id=room_id)
+            obj.members.add(request.user)
+            return Response({"added member"},status=status.HTTP_200_OK)
+        except Exception as e:
+            print(f"ERROR in adding member:{str(e)}")
+            return Response({f"error in adding member {str(e)}"},status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+    def delete(self,request,pk):
+        try:
+            """delete memeber to a room for authenticated user"""
+
+            room_id=pk
+            obj=Room.objects.get(id=room_id)
+            obj.members.remove(request.user)
+            return Response({"deleted member"},status=status.HTTP_200_OK)
+        except Exception as e:
+            print(f"ERROR in deleting member:{str(e)}")
+            return Response({f"error in deleting member {str(e)}"},status=status.HTTP_400_BAD_REQUEST)
+
+
+
+        
